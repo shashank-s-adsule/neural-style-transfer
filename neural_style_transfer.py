14bc83cfffc801fa9fa46ceb4d106ab2598b7eff
Shashank Satish Adsule <shashank.s.adsule@gmail.com>
2024-07-31 02:02:22 +0530

    --update

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -10,6 +10,7 @@ def argument():
     arg=argparse.ArgumentParser()
     arg.add_argument("--c_image",type=str,default="D:\\shashank\\test code\\test_repo\\neural-style-transfer\\data\\dataset\\content\\TamilContentImages\\C_image2.jpg",help="path for content image")
     arg.add_argument("--s_image",type=str,default="D:\\shashank\\test code\\test_repo\\neural-style-transfer\\data\\dataset\\styles\\Artworks\\423786.jpg",help="path for style image")
+    arg.add_argument("--device",default="cuda" if torch.cuda.is_available() else "cpu", help="set device cofigation (cuda, CPU)")
 
     return arg.parse_args()
 
